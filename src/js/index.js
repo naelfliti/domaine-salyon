b34c082dc5f11920f42cd8a4b0ae24a0ab856a0d
Nael Fliti <nanouflity@gmail.com>
2021-09-08 18:46:23 +0200

    done product section

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,11 @@
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    getDirection : true
+    getDirection : true,
+    mobile: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
 });
